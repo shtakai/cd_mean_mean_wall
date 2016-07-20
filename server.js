71be0046166b2d1632bfa3c27ba6c38766745430
Alyson Takai <shtakai@gmail.com>
2016-07-20 16:57:09 -0700

    add scaffodling(commented out)

--- server.js
+++ server.js
@@ -11,8 +11,22 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, './client')))
 app.use(express.static(path.join(__dirname, './bower_components')))
 
-// require('')
+require('./server/config/mongoose.js')
+require('./server/config/routes.js')(app)
 
+/**
+ * just use many users
+ */
+
+//const mongoose = require('mongoose')
+//const _ = require('lodash')
+////console.log(chance)
+//const faker = require('faker')
+//const User = mongoose.model('User')
+//_(100).times(() => {
+  //let u = new User({name: faker.commerce.productName()})
+  //u.save()
+//})
 
 const server = app.listen(port, () => {
     console.log('server start on #', port)
